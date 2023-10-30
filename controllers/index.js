d6aca009858424aaffe174a1e96a0ab9a7882605
TorySnopl <Paramedic064@gmail.com>
2023-10-30 13:32:26 -0700

    login route not working

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+const loginRoute = require('./loginRoute');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.use('/login', loginRoute);
 
 module.exports = router;
