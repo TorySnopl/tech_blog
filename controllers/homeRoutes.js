1cbc8451a6fe470c0a6645818662abd76dffe041
TorySnopl <Paramedic064@gmail.com>
2023-10-30 18:38:28 -0700

    home and login route work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,7 +9,7 @@ router.get('/',async (req,res)=>{
 
         const posts = postData.map((post)=> post.get({plain: true}));
 
-        res.render('homepage',{
+        res.render('homepage1',{
             posts,
         });
     } catch (err) {
@@ -18,5 +18,12 @@ router.get('/',async (req,res)=>{
     }
    
 });
+
+
+router.get('/login', (req,res)=>{
+
+    res.render('login');
+}
+);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
-
-const loginRoute = require('./loginRoute');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
-router.use('/login', loginRoute);
+
 
 module.exports = router;
