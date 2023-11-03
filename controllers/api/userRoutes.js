63a56ebd472ff036ea8e3a9230dbd1bb1af46a54
TorySnopl <Paramedic064@gmail.com>
2023-11-02 17:43:48 -0700

    added bcrypt and hooks to users model

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -45,6 +45,7 @@ router.post('/login', async (req, res) => {
     });
 
   } catch (err) {
+    console.error(err);
     res.status(400).json(err);
   }
 });
--- models/users.js
+++ models/users.js
@@ -1,8 +1,12 @@
 const {Model, Datatypes, DataTypes} = require('sequelize');
-
+const bcrypt = require ('bcrypt')
 const sequelize = require('../config/connection');
 
-class Users extends Model {}
+class Users extends Model {
+    checkPassword(loginPw){
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 Users.init(
     {
@@ -30,7 +34,16 @@ Users.init(
             defaultValue: DataTypes.NOW,
         },
     },
-    {
+    { hooks: {
+        beforeCreate: async (newUserData) => {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+        beforeUpdate: async (updatedUserData) => {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        },
+      },
         sequelize,
         modelName: 'Users',
         timestamps: true,
